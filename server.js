c0849b1860a81d267c2f04c783675a38a3e9ca86
Andrew Bliach <ambliach@gmail.com>
2019-05-29 18:22:25 -0400

    Progress2

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use(express.static("app/public"))
+
+require("../FriendFinder/app/routing/apiRoutes")(app)
+require("../FriendFinder/app/routing/htmlRoutes")(app)
+
+
+
+app.listen(PORT, function(){
+    console.log("App listening on PORT: " + PORT)
+})
